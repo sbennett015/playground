f3c022edf12a5c20c67794a4e78c78077968501f
sbennett015 <85761932+sbennett015@users.noreply.github.com>
2021-07-29 13:50:48 -0700

    Create playground.py

---
+++ playground.py
@@ -0,0 +1,18 @@
+# function that takes in a locked pdf file, brute forces the passcode, and returns the unlocked file
+def find_password(file):
+    print("hi")
+
+password: str = "a1b2c3"
+
+
+def crack():
+    for char in "abcdefjhijklmnopqrstuvwxyz0123456789":
+        if (char == password)
+            return password
+        else:
+            crack()
+
+  
+  
+
+  
